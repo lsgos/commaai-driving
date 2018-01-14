3bcafd98fe0ed4f829d908cc8c341d1df1075a46
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2018-01-14 15:28:17 +0000

    Added a version of the comma model with concrete dropout applied

--- load_steering_model.py
+++ load_steering_model.py
@@ -9,37 +9,94 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
 from keras.layers import Conv2D
 from keras import backend as K
+import keras
+from concrete_dropout import ConcreteDropout
+
 
 def define_model(time_len=1):
-  """ 
-  This is the model definition for the pre-trained comma-ai
-  steering prediction model. It has been changed a little to be up to
-  date with the newer keras API. I couldn't get loading it from the
-  JSON to work properly, but this should be exactly equivalent in any
-  case
-  """
+    """ 
+    This is the model definition for the pre-trained comma-ai
+    steering prediction model. It has been changed a little to be up to
+    date with the newer keras API. I couldn't get loading it from the
+    JSON to work properly, but this should be exactly equivalent in any
+    case
+    """
 
-  K.set_image_data_format('channels_first')
-  ch, row, col = 3, 160, 320  # camera format
+    K.set_image_data_format('channels_first')
+    ch, row, col = 3, 160, 320  # camera format
 
-  model = Sequential()
-  model.add(Lambda(lambda x: x/127.5 - 1.,
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
             input_shape=(ch, row, col),
             output_shape=(ch, row, col)))
-  model.add(Conv2D(16, (8, 8), strides=(4, 4), padding="same"))
-  model.add(ELU())
-  model.add(Conv2D(32, (5, 5),strides=(2, 2), padding="same"))
-  model.add(ELU())
-  model.add(Conv2D(64, (5, 5), strides=(2, 2), padding="same"))
-  model.add(Flatten())
-  model.add(Dropout(.2))
-  model.add(ELU())
-  model.add(Dense(512))
-  model.add(Dropout(.5))
-  model.add(ELU())
-  model.add(Dense(1))
+    model.add(Conv2D(16, (8, 8), strides=(4, 4), padding="same"))
+    model.add(ELU())
+    model.add(Conv2D(32, (5, 5),strides=(2, 2), padding="same"))
+    model.add(ELU())
+    model.add(Conv2D(64, (5, 5), strides=(2, 2), padding="same"))
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(512))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(1))
 
-  model.compile(optimizer="adam", loss="mse")
+    model.compile(optimizer="adam", loss="mse")
 
-  return model
+    return model
 
+def define_cdropout_model():
+    """
+    This is a version of the model used by the comma ai people, but with dropout
+    fc layers replaced by concrete dropout layers
+    """
+    # 7 hours of 20 hz video.
+    N_DATA = 20 * 7 * 60 ** 2
+    LENGTH_SCALE = 1e-2 
+    MODEL_PRECISION = 1e-1
+    WEIGHT_DECAY = LENGTH_SCALE ** 2 / (2 * N_DATA * MODEL_PRECISION)
+    WEIGHT_REGULARIZER =  LENGTH_SCALE ** 2 / (N_DATA * MODEL_PRECISION)
+    DROPOUT_REGULARIZER = 1 / (MODEL_PRECISION * N_DATA)
+
+    K.set_image_data_format('channels_first')
+    ch, row, col = 3, 160, 320  # camera format
+  
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+              input_shape=(ch, row, col),
+              output_shape=(ch, row, col)))
+    model.add(Conv2D(16, (8, 8), strides=(4, 4), padding="same"))
+    model.add(ELU())
+    model.add(Conv2D(32, (5, 5),strides=(2, 2), padding="same"))
+    model.add(ELU())
+    model.add(Conv2D(64, (5, 5), strides=(2, 2), padding="same"))
+    model.add(Flatten())
+    model.add(ELU())
+    model.add(ConcreteDropout(Dense(512, activation='elu'),
+                              weight_regularizer=WEIGHT_REGULARIZER,
+                              dropout_regularizer=DROPOUT_REGULARIZER))
+    model.add(ELU())
+    model.add(ConcreteDropout(Dense(1),
+                              weight_regularizer=WEIGHT_REGULARIZER,
+                              dropout_regularizer=DROPOUT_REGULARIZER))
+  
+    model.compile(optimizer="adam", loss="mse")
+  
+    return model
+  
+ 
+def define_linear_model(time_len=1):
+    """
+    Sanity check; just do least-squares regression and see what performance we get
+    """
+    K.set_image_data_format('channels_first')
+    ch, row, col = 3, 160, 320  # camera format
+    weight_decay = 0.0001
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(ch, row, col),
+            output_shape=(ch, row, col)))
+    model.add(Flatten())   
+    model.add(Dense(1, kernel_regularizer=keras.regularizers.l2(weight_decay)))
+    return model
--- train_steering_model.py
+++ train_steering_model.py
@@ -10,6 +10,7 @@ from keras.layers import Dense, Dropout, Flatten, Lambda, ELU, Conv2D
 from keras import backend as K
 from server import client_generator
 import keras
+from load_steering_model import define_model, define_linear_model, define_cdropout_model
 
 def gen(hwm, host, port):
   for tup in client_generator(hwm=hwm, host=host, port=port):
@@ -19,85 +20,54 @@ def gen(hwm, host, port):
       X = X[:, -1]
     yield X, Y
 
-
-def get_model(time_len=1):
-  K.set_image_data_format('channels_first') #this has been changed since this code was written
-  ch, row, col = 3, 160, 320  # camera format
-
-  weight_decay = 0.0001
-  model = Sequential()
-  model.add(Lambda(lambda x: x/127.5 - 1.,
-            input_shape=(ch, row, col),
-            output_shape=(ch, row, col)))
-  model.add(Conv2D(16, (8, 8), strides=(4, 4), padding="same", kernel_regularizer=keras.regularizers.l2(weight_decay)))
-  model.add(ELU())
-  model.add(Conv2D(32, (5, 5),strides=(2, 2), padding="same", kernel_regularizer=keras.regularizers.l2(weight_decay)))
-  model.add(ELU())
-  model.add(Conv2D(64, (5, 5), strides=(2, 2), padding="same", kernel_regularizer=keras.regularizers.l2(weight_decay)))
-  model.add(Flatten())
-  model.add(Dropout(.2))
-  model.add(ELU())
-  model.add(Dense(512, kernel_regularizer=keras.regularizers.l2(weight_decay)))
-  model.add(Dropout(.5))
-  model.add(ELU())
-  model.add(Dense(1, kernel_regularizer=keras.regularizers.l2(weight_decay)))
-
-
-  model.compile(optimizer="adam", loss="mse")
-
-  return model
-
-def get_linear_model(time_len=1):
-    """
-    Sanity check; just do least-squares regression and see what performance we get
-    """
-    K.set_image_data_format('channels_first')
-    ch, row, col = 3, 160, 320  # camera format
-    weight_decay = 0.0001
-    model = Sequential()
-    model.add(Lambda(lambda x: x/127.5 - 1.,
-            input_shape=(ch, row, col),
-            output_shape=(ch, row, col)))
-    model.add(Flatten())   
-    model.add(Dense(1, kernel_regularizer=keras.regularizers.l2(weight_decay)))
-    return model
+def get_conv_layers(model):
+    return [i for i in range(len(model.layers)) if 'conv' in model.layers[i].name]
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Steering angle model trainer')
-  parser.add_argument('--host', type=str, default="localhost", help='Data server ip address.')
-  parser.add_argument('--port', type=int, default=5557, help='Port of server.')
-  parser.add_argument('--val_port', type=int, default=5556, help='Port of server for validation dataset.')
-  parser.add_argument('--batch', type=int, default=256, help='Batch size.')
-  parser.add_argument('--epoch', type=int, default=200, help='Number of epochs.')
-  parser.add_argument('--epochsize', type=int, default=10000, help='How many frames per epoch.')
-  parser.add_argument('--skipvalidate', dest='skipvalidate', action='store_true', help='Multiple path output.')
-  parser.add_argument('--use_previous', type=bool,default=False, help='Start training from the pre-trained weights')
-  parser.add_argument('--use_linear', type=bool, default=False, help='Train a linear model as a sanity check')
-  parser.set_defaults(skipvalidate=False)
-  parser.set_defaults(loadweights=False)
-  args = parser.parse_args()
-  if args.use_linear:
-      args.use_previous=False
-  if args.use_linear: 
-    model = get_linear_model()
-  else:
-    model = get_model()
-  if os.path.exists('./outputs/steering_model') and args.use_previous:
-      model.load_weights('./outputs/steering_model/steering_angle.keras')
-  model.fit_generator(
-    gen(20, args.host, port=args.port),
-    steps_per_epoch=10000 / args.batch, 
-    epochs=args.epoch,
-    validation_data=gen(20, args.host, port=args.val_port),
-    validation_steps=1000 / args.batch
-  )
-  print("Saving model weights and configuration file.")
+    parser = argparse.ArgumentParser(description='Steering angle model trainer')
+    parser.add_argument('--host', type=str, default="localhost", help='Data server ip address.')
+    parser.add_argument('--port', type=int, default=5557, help='Port of server.')
+    parser.add_argument('--val_port', type=int, default=5556, help='Port of server for validation dataset.')
+    parser.add_argument('--batch', type=int, default=256, help='Batch size.')
+    parser.add_argument('--epoch', type=int, default=200, help='Number of epochs.')
+    parser.add_argument('--epochsize', type=int, default=10000, help='How many frames per epoch.')
+    parser.add_argument('--skipvalidate', dest='skipvalidate', action='store_true', help='Multiple path output.')
+    parser.add_argument('--use_previous', type=bool,
+                        default=False, help='Start training from the pre-trained weights')
+    parser.add_argument('--use_model', default='default', help='Train a linear model as a sanity check')
+    parser.set_defaults(skipvalidate=False)
+    parser.set_defaults(loadweights=False)
+    args = parser.parse_args()
+    if args.use_model == 'linear':
+        args.use_previous=False
+        model = define_linear_model()
+    elif args.use_model == 'cdropout':
+        model = define_cdropout_model()
 
-  if not os.path.exists("./outputs/steering_model"):
-      os.makedirs("./outputs/steering_model")
-  if not args.use_linear:
-      model.save_weights("./outputs/steering_model/steering_angle_new.keras", True)
-      with open('./outputs/steering_model/steering_angle.json', 'w') as outfile:
-        json.dump(model.to_json(), outfile)
-  else:
-      model.save_weights('./outputs/steering_model/steering_angle_linear.keras', True)
+    else:
+        model = define_model()
+    if os.path.exists('./outputs/steering_model') and args.use_previous:
+        if args.use_model = 'default':
+            model.load_weights('./outputs/steering_model/steering_angle.keras')
+        elif args.use_model = 'cdropout':
+            prev_model = define_model()
+            prev_model.load_weights('./outputs/steering_model/steering_angle.keras')
+            for i in get_conv_layers(model):
+              model.layers[i].set_weights(
+                prev_model.layers[i].get_weights()
+                )
+
+              
+    model.fit_generator(
+        gen(20, args.host, port=args.port),
+        steps_per_epoch=10000 / args.batch, 
+        epochs=args.epoch,
+        validation_data=gen(20, args.host, port=args.val_port),
+        validation_steps=1000 / args.batch
+    )
+    print("Saving model weights and configuration file.")
+
+    if not os.path.exists("./outputs/steering_model_new"):
+        os.makedirs("./outputs/steering_model_new")
+
+    model.save_weights("./outputs/steering_model_new/steering_angle_" + args.use_model + ".keras", True)
